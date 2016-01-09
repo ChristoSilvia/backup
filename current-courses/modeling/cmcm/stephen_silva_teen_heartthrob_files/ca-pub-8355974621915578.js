7dd1eed14c0111d0e5deb51cccf2038948715820
Christopher Silvia <christopher.silvia@gmail.com>
2016-01-09 14:58:32 -0500

    removed many extraneos files

--- current-courses/modeling/cmcm/stephen_silva_teen_heartthrob_files/ca-pub-8355974621915578.js
+++
@@ -1,1 +0,0 @@
-(google_async_config = window.google_async_config || {})['ca-pub-8355974621915578'] = {"sra_enabled":false};
--- current-courses/modeling/cmcm/stephen_silva_teen_heartthrob_files/if_data/click.js
+++
@@ -1,8 +0,0 @@
-document.write("<SCR"+"IPT language='JavaScript1.1' SRC=\"https://ad.doubleclick.net/ddm/adj/N5631.1820854DIGICULTURAL.WIX.CO/B9153018.124228283;sz=300x250;click=https%3A//secserv.adtech.de/adlink%2F1442%2F5896452%2F0%2F170%2FAdId%3D13421610%3BBnId%3D2%3Bitime%3D36758459%3Blink%3Dhttp%3A%2F%2Fnym1%2Eib%2Eadnxs%2Ecom%2Fclick%3FQlD2viYE1T%2D0yHa%2DnxrPP7TIdr6fGs8%5F4QsRyEBa4z%2DYjEDC7ybqP0xG7V2gfmQ7GKPB3ZdG1FWWSU9WAAAAALtQSQBECwAAMQgAAAIAAAA4TzsC5q4JAAAAAQBVU0QAVVNEACwB%2DgD%2D7gAAXKYDAQUAAQAAAAAACyjdAgAAAAA%2E%2Fcnd%3D%21lQZtOwiiwp4FELie7REY5t0mIAA%2E%2Freferrer%3Dhttp%3A%2F%2Fwww%2Etv5%2Ecom%2Eph%2F%2Fclickenc%3D;ord=36758459?\">\n");
-document.write("</SCR"+"IPT>\n");
-document.write("<NOSCRIPT>\n");
-document.write("<A HREF=\"https://secserv.adtech.de/adlink/1442/5896452/0/170/AdId=13421610;BnId=2;itime=36758459;nodecode=yes;link=http://nym1.ib.adnxs.com/click?QlD2viYE1T-0yHa-nxrPP7TIdr6fGs8_4QsRyEBa4z-YjEDC7ybqP0xG7V2gfmQ7GKPB3ZdG1FWWSU9WAAAAALtQSQBECwAAMQgAAAIAAAA4TzsC5q4JAAAAAQBVU0QAVVNEACwB-gD-7gAAXKYDAQUAAQAAAAAACyjdAgAAAAA./cnd=%21lQZtOwiiwp4FELie7REY5t0mIAA./referrer=http%3A%2F%2Fwww.tv5.com.ph%2F/clickenc=https://ad.doubleclick.net/ddm/jump/N5631.1820854DIGICULTURAL.WIX.CO/B9153018.124228283;sz=300x250;ord=36758459?\">\n");
-document.write("<IMG SRC=\"https://ad.doubleclick.net/ddm/ad/N5631.1820854DIGICULTURAL.WIX.CO/B9153018.124228283;sz=300x250;ord=36758459?\" BORDER=0 WIDTH=300 HEIGHT=250 ALT=\"Advertisement\"></A>\n");
-document.write("</NOSCRIPT>\n");
-
-
